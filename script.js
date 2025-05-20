d84a1fbe293fd194eadee450e8b8af50e8e23b2c
Martincho246 <141275657+Martincho246@users.noreply.github.com>
2025-05-20 20:49:14 -0300

    modificacion nombres y selector

--- script.js
+++ script.js
@@ -19,19 +19,39 @@ navLinks.forEach(link => {
         }
     });
 });
+
+  // Mostrar u ocultar campos "Otro"
+  document.getElementById("tipoMadera").addEventListener("change", function () {
+    const otroTipoMaderaContainer = document.getElementById("otroTipoMaderaContainer");
+    otroTipoMaderaContainer.style.display = this.value === "Otro" ? "block" : "none";
+  });
+
+  document.getElementById("tipoSector").addEventListener("change", function () {
+    const otroSectorContainer = document.getElementById("otroSectorContainer");
+    otroSectorContainer.style.display = this.value === "Otro" ? "block" : "none";
+  });
+
 document.getElementById('formularioPedido').addEventListener('submit', function (e) {
     e.preventDefault();
   
-    const tipo = document.getElementById('tipoMadera').value;
-    const otro = document.getElementById('otroTipoMadera').value.trim();
-    let tipoFinal = tipo === 'Otro' ? otro : tipo;
-  
-    if (tipo === 'Otro' && otro === '') {
+    const tipoMad = document.getElementById('tipoMadera').value;
+    const otroTipMad = document.getElementById('otroTipoMadera').value.trim();
+    let tipoFinalMad = tipoMad === 'Otro' ? otroTipMad : tipoMad;
+
+    const tipSect = document.getElementById('tipoSector').value;
+    const otroSect = document.getElementById('otroSector').value.trim();
+    let tipoFinalSect = tipSect === 'Otro' ? otroSect : tipSect;
+
+    if (tipoMad === 'Otro' && otroTipMad === '') {
       alert("Por favor completá el tipo de madera.");
       return;
     }
+    if (tipSect === 'Otro' && otroSect === '') {
+      alert("Por favor completá el sector.");
+      return;
+    }
   
-    const mensaje = `Hola! Quiero solicitar un mueble de madera tipo: ${tipoFinal}`;
+    const mensaje = `Buen día! Quiero solicitar un mueble de tipo: ${tipoFinalMad}, para ${tipoFinalSect}`;
     const telefono = '3492327644';
     const url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
     window.open(url, '_blank');
